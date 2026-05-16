172349716c538e259a8f8bd7566d2ee780152957
DVZakharov <zaharovd840@gmail.com>
2026-05-16 11:16:34 +0700

    adasdda

---
+++ Implementations/LongLongComparisonOperators.cs
@@ -0,0 +1,65 @@
+﻿using System.Numerics;
+
+namespace CompositeLong;
+
+public partial class LongLong : IComparisonOperators<LongLong, LongLong, bool>
+{
+    public override bool Equals(object? obj)
+    {
+        if (base.Equals(obj))
+            return true;
+
+        if (obj is LongLong comLong)
+        {
+            return
+                LowCoef == comLong.LowCoef
+                && HighCoef == comLong.HighCoef
+                && IsNegative == comLong.IsNegative;
+        }
+        return false;
+    }
+
+    public static bool operator <(LongLong left, LongLong right)
+    {
+        if (left.IsNegative != right.IsNegative)
+            return left.IsNegative;
+
+        if (left.IsNegative)
+        {
+            if (left.HighCoef != right.HighCoef)
+                return left.HighCoef > right.HighCoef;
+            return left.LowCoef > right.LowCoef;
+        }
+        else
+        {
+            if (left.HighCoef != right.HighCoef)
+                return left.HighCoef < right.HighCoef;
+            return left.LowCoef < right.LowCoef;
+        }
+    }
+
+    public static bool operator >(LongLong left, LongLong right)
+    {
+        return right < left;
+    }
+
+    public static bool operator >=(LongLong left, LongLong right)
+    {
+        return (left > right) || left.Equals(right);
+    }
+
+    public static bool operator <=(LongLong left, LongLong right)
+    {
+        return right >= left;
+    }
+
+    public static bool operator ==(LongLong? left, LongLong? right)
+    {
+        return left!.Equals(right);
+    }
+
+    public static bool operator !=(LongLong? left, LongLong? right)
+    {
+        return !left!.Equals(right);
+    }
+}
---
+++ LongLong.cs
@@ -0,0 +1,105 @@
+﻿namespace CompositeLong;
+
+public partial class LongLong
+{
+    public LongLong()
+    {
+        LowCoef = 0;
+        HighCoef = 0;
+        IsNegative = false;
+    }
+
+    public LongLong(string value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value.Length == 0)
+            throw new ArgumentException("Строка не может быть пустой.", nameof(value));
+
+        int end = value.Length - 1;
+        while (end >= 0 && char.IsWhiteSpace(value[end]))
+            end--;
+        int start = 0;
+        while (start <= end && char.IsWhiteSpace(value[start]))
+            start++;
+
+        if (start > end)
+            throw new FormatException("Строка содержит только пробельные символы.");
+
+        if (value[start] == '-')
+        {
+            IsNegative = true;
+            start++;
+        }
+        else if (value[start] == '+')
+        {
+            start++;
+        }
+
+        while (start <= end && value[start] == '0')
+            start++;
+
+        if (start > end)
+        {
+            HighCoef = 0;
+            LowCoef = 0;
+            IsNegative = false;
+            return;
+        }
+
+        UInt128 result = 0;
+
+        for (int i = start; i <= end; i++)
+        {
+            char c = value[i];
+            if (c < '0' || c > '9')
+                throw new FormatException($"Некорректный символ '{c}' в позиции {i}.");
+
+            uint digit = (uint)(c - '0');
+
+            if (result > (UInt128.MaxValue - digit) / 10)
+                throw new OverflowException("Число превышает диапазон 128-битного целого.");
+
+            result = result * 10 + digit;
+        }
+
+        LowCoef = (ulong)result;
+        HighCoef = (long)(result >> 64);
+
+        if (result == 0)
+            IsNegative = false;
+    }
+
+    public long HighCoef { get; init; }
+    public ulong LowCoef { get; init; }
+    public bool IsNegative { get; init; }
+
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(HighCoef, LowCoef, IsNegative);
+    }
+
+
+    public override string ToString()
+    {
+        if (HighCoef == 0 && LowCoef == 0)
+            return "0";
+
+        LongLong absValue = this.IsNegative ? -this : this;
+
+        var digits = new List<char>();
+
+        while (!(absValue.HighCoef == 0 && absValue.LowCoef == 0))
+        {
+            LongLong remainder = absValue % 10;
+            char digitChar = (char)('0' + remainder.LowCoef);
+            digits.Add(digitChar);
+
+            absValue /= 10;
+        }
+        digits.Reverse();
+
+        string result = new([.. digits]);
+        return this.IsNegative ? "-" + result : result;
+    }
+}
